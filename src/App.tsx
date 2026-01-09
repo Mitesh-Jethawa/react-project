3078a92a43c340e9d68d7ce15ad0f7f2091f1424
Shubh <shubhsinghmoriya9816@gmail.com>
2026-01-09 11:02:55 +0530

    Contexts

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,12 @@ const App = () => {
   const headerRef = useRef<HTMLElement | null>(null);
   const [headerHeight, setHeaderHeight] = useState(0);
   const [editorText, setEditorText] = useState("");
-
+  const [text, setText] = useState<string>("")
+  const [highlightedHtml, setHighlightedHtml] = useState<string>("")
+  const [biases, setBiases] = useState<string[]>([])
+  const [score, setScore] = useState<number>(0)
+  const [pronounStats, setPronounStats] = useState<Record<string, number>>({})
+  
   useLayoutEffect(() => {
     if (headerRef.current) {
       setHeaderHeight(headerRef.current.offsetHeight);
@@ -17,16 +22,23 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    if (!editorText) return;
-    analyzeText(editorText)
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
-  }, [editorText]);
+  if (!editorText) return;
+
+  analyzeText(editorText)
+    .then((res) => {
+      setText(res.text)
+      setHighlightedHtml(res.highlighted_html)
+      setBiases(res.biases)
+      setScore(res.score)
+      setPronounStats(res.pronoun_stats)
+    })
+    .catch((err) => console.error(err))
+}, [editorText])
 
   return (
     <>
       <LayoutContext.Provider
-        value={{ headerHeight, editorText, setEditorText }}
+        value={{ headerHeight, editorText, setEditorText, text ,highlightedHtml, biases, score, pronounStats  }}
       >
         <Header
           ref={headerRef}
--- src/context/LayoutContext.tsx
+++ src/context/LayoutContext.tsx
@@ -4,12 +4,23 @@ interface LayoutContextProps {
   headerHeight: number;
   editorText: string;
   setEditorText: (text: string) => void;
+  text : string;
+  highlightedHtml : string;
+  biases : string[];
+  score: number
+  pronounStats: Record<string, number>
 }
 
 export const LayoutContext = createContext<LayoutContextProps>({
   headerHeight: 0,
   editorText: "",
   setEditorText: () => {},
+  text : "",
+  highlightedHtml : "",
+  biases : [],
+  score: 0,
+  pronounStats: {},
+
 });
 
 
